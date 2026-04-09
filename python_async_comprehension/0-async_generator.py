737bb5efc7d2bc1888ecbd4f66f892feaf06b970
frxjoan <12224@holbertonstudents.com>
2026-04-09 09:37:10 +0200

    type annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Module containing an asynchronous generator that yields random numbers."""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Asynchronous generator that loops 10 times, each time asynchronously
     waits 1 second, then yields a random float between 0 and 10.
     """
