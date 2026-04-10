efbd9479a5de661def0fc78525a88653daddaf49
frxjoan <12224@holbertonstudents.com>
2026-04-10 12:48:01 +0200

    turn asyncgen into gen

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module containing an asynchronous generator that yields random numbers."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Asynchronous generator that loops 10 times, each time asynchronously
     waits 1 second, then yields a random float between 0 and 10.
     """
