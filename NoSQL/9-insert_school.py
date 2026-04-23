b49b28a040de24553e6fbf0655b0cb9d626f4f73
frxjoan <12224@holbertonstudents.com>
2026-04-23 08:47:24 +0200

    9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 9-insert_school """
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert a new document in a collection based on kwargs"""
+    return mongo_collection.insert_one(kwargs).inserted_id
